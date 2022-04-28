cd9567ae00c9c7f93bcca470230f104e49140274
Aurelius Prochazka <aurephd@gmail.com>
2022-04-27 22:35:44 -0700

    Cleanup

--- Package.swift
+++ Package.swift
@@ -5,24 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Tonic",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Tonic",
-            targets: ["Tonic"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    products: [.library(name: "Tonic", targets: ["Tonic"])],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Tonic",
-            dependencies: []),
-        .testTarget(
-            name: "TonicTests",
-            dependencies: ["Tonic"]),
+        .target(name: "Tonic", dependencies: []),
+        .testTarget(name: "TonicTests", dependencies: ["Tonic"]),
     ]
 )
