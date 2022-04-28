f85b72ff71804e393d1d84da669ca92d54b1996e
Taylor Holliday <taylor@audulus.com>
2022-04-28 13:10:22 -0700

    Create Interval.swift

---
+++ Sources/Tonic/Interval.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+
+enum Interval {
+    case d2
+    case m2
+    case m3
+    case M3
+    case P4
+    case d5
+    case P5
+    case m6
+    case M6
+    case m7
+    case M7
+}
