592e01bb99fb2b120fe696fdeeef5c7dcc59937e
Taylor Holliday <taylor@audulus.com>
2022-04-27 21:27:34 -0700

    note spelling

--- Sources/Tonic/Note.swift
+++ Sources/Tonic/Note.swift
@@ -4,9 +4,20 @@ import Foundation
 struct Note {
 
     /// 0-127 starting at C
-    var midiNoteNumber: UInt8
+    var midiNoteNumber: Int8
 
     /// Semitone shift for accidental to distinguish defferent spelling of the note.
     var accientalShift: Int8
 
+    var spelling: String {
+        let baseNote = midiNoteNumber-accientalShift
+
+        let baseSpelling = ["C", "", "D",  "","E", "F",  "","G",  "","A",  "","B"]
+        let note = baseSpelling[Int(baseNote % 12)]
+        assert(note != "")
+
+        let accidental = ["𝄫", "♭", "", "♯", "𝄪"][Int(accientalShift) + 2]
+
+        return note + accidental
+    }
 }
--- Tests/TonicTests/TonicTests.swift
+++ Tests/TonicTests/TonicTests.swift
@@ -2,6 +2,14 @@ import XCTest
 @testable import Tonic
 
 final class TonicTests: XCTestCase {
-    func testExample() throws {
+    func testNoteSpelling() {
+        let note = Note(midiNoteNumber: 60, accientalShift: 0)
+        XCTAssertEqual(note.spelling, "C")
+
+        let note2 = Note(midiNoteNumber: 61, accientalShift: -1)
+        XCTAssertEqual(note2.spelling, "D♭")
+
+        let note3 = Note(midiNoteNumber: 61, accientalShift: 1)
+        XCTAssertEqual(note3.spelling, "C♯")
     }
 }
