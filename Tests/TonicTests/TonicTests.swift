216f4af0663c4d4d11db3e7b6ee7412495743906
Taylor Holliday <taylor@audulus.com>
2022-04-28 14:23:59 -0700

    Blues, not pentatonic

--- Tests/TonicTests/TonicTests.swift
+++ Tests/TonicTests/TonicTests.swift
@@ -20,7 +20,7 @@ final class TonicTests: XCTestCase {
     }
 
     func testBluesScale() {
-        let blues = Scale(bits: 0b1001010100)
+        let blues = Scale(bits: 0b1001110100)
 
         print("blues intervals: \(blues)", blues.intervals)
     }
