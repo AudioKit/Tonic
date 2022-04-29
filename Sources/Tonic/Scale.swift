0a89eac1b4a43fb6a3a10b8449fce9f535f3a697
Taylor Holliday <taylor@audulus.com>
2022-04-28 19:54:22 -0700

    use constructor

--- Sources/Tonic/Scale.swift
+++ Sources/Tonic/Scale.swift
@@ -29,7 +29,7 @@ struct Scale: OptionSet {
     }
 
     static let pentatonicMinor = Scale(intervals: [.m3, .P4, .P5, .m7])
-    static let blues = Scale(rawValue: 0b1001110100)
+    static let blues = Scale(intervals: [.m3, .P4, .d5, .P5, .m7])
     static let minor = Scale(rawValue: 0b1011010110)
     static let major = Scale(rawValue: 0b10101011010)
     static let chromatic = Scale(rawValue: 0b11111111111)
