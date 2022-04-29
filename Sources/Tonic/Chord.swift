b8fbbbfcb8ada1a94828a5a077df4485e950ca44
Taylor Holliday <taylor@audulus.com>
2022-04-29 15:28:12 -0700

    add ChordType

--- Sources/Tonic/Chord.swift
+++ Sources/Tonic/Chord.swift
@@ -94,3 +94,36 @@ func generateTriads() -> [Chord2] {
     }
     return chords
 }
+
+/// A set of intervals from the root (tonic).
+struct ChordType: OptionSet {
+    let rawValue: Int
+
+    var intervals: [Interval] {
+
+        var result: [Interval] = []
+        for i in 0..<11 {
+            if (rawValue >> i) & 1 != 0 {
+                result.append(Interval(rawValue: i)!)
+            }
+        }
+        return result
+    }
+
+    init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+
+    init(intervals: [Interval]) {
+        var r = 0
+        for interval in intervals {
+            r |= (1 << interval.rawValue)
+        }
+        self.rawValue = r
+    }
+
+    static let majorTriad = ChordType(intervals: [.M3, .P5])
+    static let majorSixth = ChordType(intervals: [.M3, .P5, .M6])
+    static let dominantSeventh = ChordType(intervals: [.M3, .P5, .m7])
+    static let minorTriad = ChordType(intervals: [.m3, .P5])
+}
