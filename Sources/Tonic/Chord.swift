fb5c9239feed1f1440fae58afcadba5017b25053
Taylor Holliday <taylor@audulus.com>
2022-04-29 07:33:28 -0700

    Use BitSet128

--- Sources/Tonic/Chord.swift
+++ Sources/Tonic/Chord.swift
@@ -16,9 +16,7 @@ struct Chord2 {
 /// Chord addresses all possible combinations of simultaneous MIDI notes.
 struct Chord {
 
-    // Since we don't have a UInt128, we use two UInt64s.
-    private var highNotes: UInt64 = 0
-    private var lowNotes: UInt64 = 0
+    var noteSet = BitSet128()
 
     init(notes: [Note]) {
         for n in notes {
@@ -27,7 +25,7 @@ struct Chord {
     }
 
     var noteCount: Int {
-        lowNotes.nonzeroBitCount + highNotes.nonzeroBitCount
+        notes.count
     }
 
     var isDyad: Bool {
@@ -46,40 +44,19 @@ struct Chord {
 
     /// Calls a function for each note in the chord.
     func forEachNote(_ f: (Note) -> ()) {
-        for root in 0..<128 {
-            let n = Note(noteNumber: Int8(root))
-            if contains(note: n) {
-                f(n)
-            }
+        noteSet.forEach { bit in
+            f(Note(noteNumber: Int8(bit)))
         }
     }
 
     /// Add a note to a chord.
     mutating func add(note: Note) {
-        if note.noteNumber < 64 {
-            lowNotes |= 1 << note.noteNumber
-        } else {
-            highNotes |= 1 << (note.noteNumber - 64)
-        }
+        noteSet.add(bit: Int(note.noteNumber))
     }
 
     /// Is a note in a chord?
     func contains(note: Note) -> Bool {
-        if note.noteNumber < 64 {
-            return (lowNotes & (1 << note.noteNumber)) != 0
-        } else {
-            return (highNotes & (1 << (note.noteNumber - 64))) != 0
-        }
-    }
-
-    mutating func shift(semitones: Int) {
-        if semitones > 0 {
-            highNotes <<= semitones
-            // How many do we have to copy from low to high?
-            let lowToHigh = (lowNotes << (64 - semitones)) & ((1 << semitones) - 1)
-            highNotes |= lowToHigh
-            lowNotes <<= semitones
-        }
+        noteSet.isSet(bit: Int(note.noteNumber))
     }
 
     /// One-sided Hausdorff distance to another chord.
