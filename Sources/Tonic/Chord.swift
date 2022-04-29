ee939daadf7410c0472566054b121cc23a5f0e60
Taylor Holliday <taylor@audulus.com>
2022-04-28 19:25:15 -0700

    Update Chord.swift

--- Sources/Tonic/Chord.swift
+++ Sources/Tonic/Chord.swift
@@ -10,4 +10,22 @@ struct Chord {
     var isTriad: Bool {
         lowNotes.nonzeroBitCount + highNotes.nonzeroBitCount == 3
     }
+
+    /// Add a note to a chord.
+    mutating func add(note: Note) {
+        if note.noteNumber < 63 {
+            lowNotes |= 1 << note.noteNumber
+        } else {
+            highNotes |= 1 << (note.noteNumber-64)
+        }
+    }
+
+    /// Is a note in a chord?
+    func contains(note: Note) -> Bool {
+        if note.noteNumber < 63 {
+            return (lowNotes & (1 << note.noteNumber)) != 0
+        } else {
+            return (highNotes & (1 << (note.noteNumber-64))) != 0
+        }
+    }
 }
