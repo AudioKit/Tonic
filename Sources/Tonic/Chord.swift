6d3f2e121a10ee49478e458e7df0c578831116a9
Taylor Holliday <taylor@audulus.com>
2022-04-28 19:17:01 -0700

    Update Chord.swift

--- Sources/Tonic/Chord.swift
+++ Sources/Tonic/Chord.swift
@@ -1,24 +1,16 @@
 
 import Foundation
 
-let m1: UInt64 = 0x5555555555555555 // binary: 0101...
-let m2: UInt64 = 0x3333333333333333 // binary: 00110011..
-let m4: UInt64 = 0x0f0f0f0f0f0f0f0f // binary:  4 zeros,  4 ones ...
-
 // See https://en.wikipedia.org/wiki/Hamming_weight
-// This uses fewer arithmetic operations than any other known
-// implementation on machines with slow multiplication.
-// This algorithm uses 17 arithmetic operations.
-func popcount64b(_ xx: UInt64) -> UInt64
+func popcount64b(_ xx: UInt64) -> Int
 {
     var x = xx
-    x -= (x >> 1) & m1;             //put count of each 2 bits into those 2 bits
-    x = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits
-    x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits
-    x += x >>  8;  //put count of each 16 bits into their lowest 8 bits
-    x += x >> 16;  //put count of each 32 bits into their lowest 8 bits
-    x += x >> 32;  //put count of each 64 bits into their lowest 8 bits
-    return x & 0x7f;
+    var count = 0;
+    while x != 0 {
+        x &= x - 1
+        count += 1
+    }
+    return count
 }
 
 struct Chord {
