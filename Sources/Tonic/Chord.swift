5cf65d552bf5e52d2891a83c4edfa059dfd6bde2
Taylor Holliday <taylor@audulus.com>
2022-04-28 19:21:05 -0700

    cleanup

--- Sources/Tonic/Chord.swift
+++ Sources/Tonic/Chord.swift
@@ -1,18 +1,6 @@
 
 import Foundation
 
-// See https://en.wikipedia.org/wiki/Hamming_weight
-func popcount64(_ xx: UInt64) -> Int
-{
-    var x = xx
-    var count = 0;
-    while x != 0 {
-        x &= x - 1
-        count += 1
-    }
-    return count
-}
-
 struct Chord {
 
     // Since we don't have a UInt128
@@ -20,6 +8,6 @@ struct Chord {
     var lowNotes: UInt64
 
     var isTriad: Bool {
-        popcount64(lowNotes) + popcount64(highNotes) == 3
+        lowNotes.nonzeroBitCount + highNotes.nonzeroBitCount == 3
     }
 }
