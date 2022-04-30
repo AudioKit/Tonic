af268edd7f2c7f3d9044fd0e07fbda0d3a057183
Aurelius Prochazka <aurephd@gmail.com>
2022-04-30 00:10:15 -0700

    Removed circle of fifths

--- Sources/Tonic/Key.swift
+++ Sources/Tonic/Key.swift
@@ -24,38 +24,3 @@ struct Key {
 
     }
 }
-
-enum CircleOfFifths: Int {
-    case Cb
-    case Gb
-    case Db
-    case Ab
-    case Eb
-    case Bb
-    case F
-    case C
-    case G
-    case D
-    case A
-    case E
-    case B
-    case Fs
-    case Cs
-
-    case ab
-    case eb
-    case bb
-    case f
-    case c
-    case g
-    case d
-    case a
-    case e
-    case b
-    case fs
-    case cs
-    case gs
-    case ds
-    case `as`
-
-}
