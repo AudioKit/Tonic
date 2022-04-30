f86f9ff57397090fbc826dfb420651697170e7b2
Taylor Holliday <taylor@audulus.com>
2022-04-30 15:09:30 -0700

    Add PitchSet

---
+++ Sources/Tonic/PitchSet.swift
@@ -0,0 +1,33 @@
+
+import Foundation
+
+/// A set of pitches represented by a bit set.
+public struct PitchSet: Hashable, Equatable {
+    var bits = BitSet128()
+
+    public init() { }
+
+    public init(pitches: [Pitch]) {
+        for pitch in pitches {
+            add(pitch: pitch)
+        }
+    }
+
+    public mutating func add(pitch: Pitch) {
+        bits.add(bit: Int(pitch.midiNoteNumber))
+    }
+
+    public func contains(pitch: Pitch) -> Bool {
+        bits.isSet(bit: Int(pitch.midiNoteNumber))
+    }
+
+    public func forEachPitch(f: (Pitch) -> ()) {
+        bits.forEach { noteIndex in
+            f(Pitch(Int8(noteIndex)))
+        }
+    }
+
+    public var count: Int {
+        bits.count
+    }
+}
