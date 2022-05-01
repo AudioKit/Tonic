1f397174330404b7d3714d0993790e041ac08b2b
Aurelius Prochazka <aurephd@gmail.com>
2022-04-30 18:28:19 -0700

    Moving syntactical sugar stuff out of Note file

---
+++ Sources/Tonic/Note+SyntacticalSugar.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+extension Note {
+    public static var Cb = Note(.C, accidental: .flat)
+    public static var C = Note(.C)
+    public static var Cs = Note(.C, accidental: .sharp)
+
+    public static var Db = Note(.D, accidental: .flat)
+    public static var D = Note(.D)
+    public static var Ds = Note(.D, accidental: .sharp)
+
+    public static var Eb = Note(.E, accidental: .flat)
+    public static var E = Note(.E)
+    public static var Es = Note(.E, accidental: .sharp)
+
+    public static var Fb = Note(.F, accidental: .flat)
+    public static var F = Note(.F)
+    public static var Fs = Note(.F, accidental: .sharp)
+
+    public static var Gb = Note(.G, accidental: .flat)
+    public static var G = Note(.G)
+    public static var Gs = Note(.G, accidental: .sharp)
+
+    public static var Ab = Note(.A, accidental: .flat)
+    public static var A = Note(.A)
+    public static var As = Note(.A, accidental: .sharp)
+
+    public static var Bb = Note(.B, accidental: .flat)
+    public static var B = Note(.B)
+    public static var Bs = Note(.B, accidental: .sharp)
+}
--- Sources/Tonic/Note.swift
+++ Sources/Tonic/Note.swift
@@ -115,36 +115,6 @@ public struct Note: Equatable, Hashable {
     public var index: Int {
         (octave+1) * 7 * 5 + letter.rawValue * 5 + (Int(accidental.rawValue)+2)
     }
-
-    public static var Cb = Note(.C, accidental: .flat)
-    public static var C = Note(.C)
-    public static var Cs = Note(.C, accidental: .sharp)
-
-    public static var Db = Note(.D, accidental: .flat)
-    public static var D = Note(.D)
-    public static var Ds = Note(.D, accidental: .sharp)
-
-    public static var Eb = Note(.E, accidental: .flat)
-    public static var E = Note(.E)
-    public static var Es = Note(.E, accidental: .sharp)
-
-    public static var Fb = Note(.F, accidental: .flat)
-    public static var F = Note(.F)
-    public static var Fs = Note(.F, accidental: .sharp)
-
-    public static var Gb = Note(.G, accidental: .flat)
-    public static var G = Note(.G)
-    public static var Gs = Note(.G, accidental: .sharp)
-
-    public static var Ab = Note(.A, accidental: .flat)
-    public static var A = Note(.A)
-    public static var As = Note(.A, accidental: .sharp)
-
-    public static var Bb = Note(.B, accidental: .flat)
-    public static var B = Note(.B)
-    public static var Bs = Note(.B, accidental: .sharp)
-
-
 }
 
 extension Note: Comparable {
@@ -152,3 +122,5 @@ extension Note: Comparable {
         (lhs.letter, lhs.accidental, lhs.octave) < (rhs.letter, rhs.accidental, rhs.octave)
     }
 }
+
+
