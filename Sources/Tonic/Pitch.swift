e4936993371d37c0ca2e173d257e31e609363f8b
Taylor Holliday <taylor@audulus.com>
2022-05-01 10:10:38 -0700

    Add comment.

--- Sources/Tonic/Pitch.swift
+++ Sources/Tonic/Pitch.swift
@@ -1,6 +1,10 @@
 
 import Foundation
 
+/// Essentially a midi note number.
+///
+/// We want to use a notion of pitch that lends itself to combinatorical algorithms,
+/// as opposed to useing e.g. a fundamental frequency.
 public struct Pitch: Equatable, Hashable {
 
     var midiNoteNumber: Int8
