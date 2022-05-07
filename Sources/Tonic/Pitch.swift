6f8ac3a685bdba2725a6bbe31f1546f0f6e0a469
Taylor Holliday <taylor@audulus.com>
2022-05-07 11:12:53 -0700

    make Pitch RawRepresentable

--- Sources/Tonic/Pitch.swift
+++ Sources/Tonic/Pitch.swift
@@ -4,9 +4,16 @@ import Foundation
 ///
 /// We want to use a notion of pitch that lends itself to combinatorial algorithms,
 /// as opposed to useing e.g. a fundamental frequency.
-public struct Pitch: Equatable, Hashable {
+public struct Pitch: Equatable, Hashable, RawRepresentable {
 
     var midiNoteNumber: Int8
+    public var rawValue: Int8 {
+        midiNoteNumber
+    }
+
+    public init(rawValue: Int8) {
+        self.midiNoteNumber = rawValue
+    }
 
     public init(_ midiNoteNumber: Int8) {
         self.midiNoteNumber = midiNoteNumber
