654b4538b86daa81db01a7a386c2f9a2b24d0130
Taylor Holliday <taylor@audulus.com>
2022-05-07 11:07:38 -0700

    Create NoteClass+Shortcuts.swift

---
+++ Sources/Tonic/NoteClass+Shortcuts.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+extension NoteClass {
+    public static var Cb = NoteClass(.C, accidental: .flat)
+    public static var C = NoteClass(.C)
+    public static var Cs = NoteClass(.C, accidental: .sharp)
+
+    public static var Db = NoteClass(.D, accidental: .flat)
+    public static var D = NoteClass(.D)
+    public static var Ds = NoteClass(.D, accidental: .sharp)
+
+    public static var Eb = NoteClass(.E, accidental: .flat)
+    public static var E = NoteClass(.E)
+    public static var Es = NoteClass(.E, accidental: .sharp)
+
+    public static var Fb = NoteClass(.F, accidental: .flat)
+    public static var F = NoteClass(.F)
+    public static var Fs = NoteClass(.F, accidental: .sharp)
+
+    public static var Gb = NoteClass(.G, accidental: .flat)
+    public static var G = NoteClass(.G)
+    public static var Gs = NoteClass(.G, accidental: .sharp)
+
+    public static var Ab = NoteClass(.A, accidental: .flat)
+    public static var A = NoteClass(.A)
+    public static var As = NoteClass(.A, accidental: .sharp)
+
+    public static var Bb = NoteClass(.B, accidental: .flat)
+    public static var B = NoteClass(.B)
+    public static var Bs = NoteClass(.B, accidental: .sharp)
+}
