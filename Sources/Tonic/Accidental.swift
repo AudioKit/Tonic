6bdfa82f22030daf66916166442e28986a2503c3
Aurelius Prochazka <aurephd@gmail.com>
2022-05-08 14:10:22 -0700

    Created files for Letter, Accidental

--- Sources/Tonic/Accidental.swift
+++ Sources/Tonic/Accidental.swift
@@ -26,18 +26,3 @@ extension Accidental: Comparable {
     }
 
 }
-
-/// The names for the "white key" notes.
-public enum Letter: Int, CaseIterable, Equatable, Hashable {
-    case C, D, E, F, G, A, B
-
-    var baseNote: UInt8 {
-        return [0, 2, 4, 5, 7, 9, 11][rawValue]
-    }
-}
-
-extension Letter: Comparable {
-    public static func < (lhs: Letter, rhs: Letter) -> Bool {
-        lhs.rawValue < rhs.rawValue
-    }
-}
