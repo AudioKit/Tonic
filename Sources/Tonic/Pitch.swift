dcca21ae9156f9c9bb026e2e59d6f6567cd42c16
Taylor Holliday <taylor@audulus.com>
2022-06-01 16:10:57 -0700

    Update Pitch.swift

--- Sources/Tonic/Pitch.swift
+++ Sources/Tonic/Pitch.swift
@@ -2,8 +2,8 @@ import Foundation
 
 public typealias PitchSet = BitSetAdapter<Pitch, BitSet128>
 
-extension PitchSet {
-    public func chord(in key: Key) -> Chord? {
+public extension PitchSet {
+    func chord(in key: Key) -> Chord? {
         var notes = NoteSet()
         forEach { notes.add($0.note(in: key))}
         return Chord(noteSet: notes)
@@ -23,17 +23,17 @@ public struct Pitch: Equatable, Hashable {
     }
 
     /// If we have a Key, we can turn a Pitch into a Note.
-    func note(in key: Key) -> Note {
+    public func note(in key: Key) -> Note {
         Note(pitch: self, key: key)
     }
 
     /// Returns the distance between Pitches in semitones.
-    func semitones(to: Pitch) -> Int8 {
+    public func semitones(to: Pitch) -> Int8 {
         abs(midiNoteNumber - to.midiNoteNumber)
     }
 
     /// Equivalence classes of pitches modulo octave.
-    var pitchClass: Int8 {
+    public var pitchClass: Int8 {
         midiNoteNumber % 12
     }
 }
