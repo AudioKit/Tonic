6de6f95ba4a66f528b50d75a63c5132179d7a15c
Aurelius Prochazka <aurephd@gmail.com>
2022-06-02 11:30:40 -0700

    Updated README by testing the claims

---
+++ Tests/TonicTests/ReadMeTests.swift
@@ -0,0 +1,42 @@
+import XCTest
+@testable import Tonic
+
+final class ReadMeTests: XCTestCase {
+
+    // What's the note for this pitch in this key?
+    func testNoteForPitchInKey() {
+        let midiNoteNumber: Int8 = 61
+        XCTAssertEqual(Note(pitch: Pitch(midiNoteNumber), key: .Bb), Note.Db)
+    }
+
+    // What's the name of a chord?
+    func testNameOfChord() {
+        let notes: [Note] = [.C, .Eb, .Gb]
+        XCTAssertEqual(Chord(notes: notes)!.description, "C°")
+    }
+
+    // What chords are in this key?
+    func testChordsInKey() {
+        XCTAssertEqual(Key.Cm.chords.count, 19)
+    }
+
+    // What chords in this key contain this note?
+    func testChordsInKeyContainNote() {
+        XCTAssertEqual(Key.C.chords.filter { $0.noteClasses.contains(.C) }.count, 9)
+    }
+
+    // What notes do these keys have in common?
+    func testCommonNotesInKeys() {
+        let commonNoteSet = Key.C.noteSet.intersection(Key.Cm.noteSet)
+        let answer: [Note] = [.C, .D, .F, .G]
+        XCTAssertEqual(commonNoteSet.array.sorted(), answer)
+    }
+
+    // What notes don't these keys have in common?
+    func testDifferentNotesInKeys() {
+        let differentNoteSet = Key.C.noteSet.symmetricDifference(Key.Cm.noteSet)
+        let answer: [Note] = [.Eb, .E, .Ab, .A, .Bb, .B]
+        XCTAssertEqual(differentNoteSet.array.sorted(), answer)
+    }
+
+}
