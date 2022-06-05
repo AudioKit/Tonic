cf9e2bcd4a01b2c326b06bb857e4b24ddc40a82a
Aurelius Prochazka <aurephd@gmail.com>
2022-06-05 14:41:34 -0700

    Use color literal

--- Sources/Tonic/PitchColor.swift
+++ Sources/Tonic/PitchColor.swift
@@ -4,19 +4,7 @@ public struct PitchColor {
 
     // Source: http://theappendix.net/posts/2013/08/music-and-color-the-french-connection
     public static var newtonian: [CGColor] {
-        [.init(red: 228/255.0, green:  53/255.0, blue:  38/255.0, alpha: 1.0),
-         .init(red: 225/255.0, green:  86/255.0, blue:  45/255.0, alpha: 1.0),
-         .init(red: 231/255.0, green: 134/255.0, blue:  53/255.0, alpha: 1.0),
-         .init(red: 239/255.0, green: 211/255.0, blue:  90/255.0, alpha: 1.0),
-         .init(red: 245/255.0, green: 244/255.0, blue:  98/255.0, alpha: 1.0),
-         .init(red:  66/255.0, green: 141/255.0, blue:  65/255.0, alpha: 1.0),
-         .init(red:  67/255.0, green: 141/255.0, blue: 128/255.0, alpha: 1.0),
-         .init(red:  26/255.0, green:  14/255.0, blue: 126/255.0, alpha: 1.0),
-         .init(red:  68/255.0, green:  20/255.0, blue: 120/255.0, alpha: 1.0),
-         .init(red: 116/255.0, green:  24/255.0, blue: 120/255.0, alpha: 1.0),
-         .init(red: 151/255.0, green:  38/255.0, blue: 131/255.0, alpha: 1.0),
-         .init(red: 197/255.0, green:  48/255.0, blue: 129/255.0, alpha: 1.0),
-        ]
+        [#colorLiteral(red: 0.8941176470588236,  green: 0.20784313725490197,  blue: 0.14901960784313725, alphas; 1.0), #colorLiteral(red: 0.8823529411764706,  green: 0.33725490196078434,  blue: 0.17647058823529413, alphas; 1.0), #colorLiteral(red: 0.9058823529411765,  green: 0.5254901960784314,   blue: 0.20784313725490197, alphas; 1.0), #colorLiteral(red: 0.9372549019607843,  green: 0.8274509803921568,   blue: 0.35294117647058826, alphas; 1.0), #colorLiteral(red: 0.9607843137254902,  green: 0.9568627450980393,   blue: 0.3843137254901961,  alphas; 1.0), #colorLiteral(red: 0.25882352941176473, green: 0.5529411764705883,   blue: 0.2549019607843137,  alphas; 1.0), #colorLiteral(red: 0.2627450980392157,  green: 0.5529411764705883,   blue: 0.5019607843137255,  alphas; 1.0), #colorLiteral(red: 0.10196078431372549, green: 0.054901960784313725, blue: 0.49411764705882355, alphas; 1.0), #colorLiteral(red: 0.26666666666666666, green: 0.0784313725490196,   blue: 0.47058823529411764, alphas; 1.0), #colorLiteral(red: 0.4549019607843137,  green: 0.09411764705882353,  blue: 0.47058823529411764, alphas; 1.0), #colorLiteral(red: 0.592156862745098,   green: 0.14901960784313725,  blue: 0.5137254901960784,  alphas; 1.0), #colorLiteral(red: 0.7725490196078432,  green: 0.18823529411764706,  blue: 0.5058823529411764,  alphas; 1.0)]
     }
 }
 
