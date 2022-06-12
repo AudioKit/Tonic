5cf4eadff6d1d60fec625038576def0d174c9a26
Aurelius Prochazka <aurephd@gmail.com>
2022-06-11 19:01:32 -0700

    Revert stray commit

--- Tests/TonicTests/IntervalTests.swift
+++ Tests/TonicTests/IntervalTests.swift
@@ -3,7 +3,6 @@ import XCTest
 
 final class IntervalTests: XCTestCase {
     func testIntervalBetween() {
-        print(Note.D - Note.C)
         XCTAssertEqual(Interval.betweenNotes(.C, .D), .M2)
         XCTAssertEqual(Interval.betweenNotes(.C, .G), .P5)
         XCTAssertEqual(Interval.betweenNotes(.G, .C), .P5)
