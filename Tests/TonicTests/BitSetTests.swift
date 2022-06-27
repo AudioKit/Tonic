b0071580c6a3e08a969abf6ca6788a111e91cf80
Taylor Holliday <taylor@audulus.com>
2022-06-27 16:32:51 -0700

    Update BitSetTests.swift

--- Tests/TonicTests/BitSetTests.swift
+++ Tests/TonicTests/BitSetTests.swift
@@ -19,4 +19,24 @@ class BitSetTests: XCTestCase {
         XCTAssertEqual(set.first, nil)
     }
 
+    func testBitSet_SetAlgebra() {
+        var set0 = BitSet512()
+        set0.add(bit: 0)
+
+        var set1 = BitSet512()
+        set1.add(bit: 1)
+
+        XCTAssertTrue(set0.contains(0))
+        XCTAssertTrue(set1.contains(1))
+
+        let union = set0.union(set1)
+        XCTAssertEqual(union.count, 2)
+
+        let isect = set0.intersection(set1)
+        XCTAssertEqual(isect.count, 0)
+
+        let diff = set0.symmetricDifference(set1)
+        XCTAssertEqual(diff.count, 2)
+    }
+
 }
