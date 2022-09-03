3665304bedbcdad3b2be86e4c89f966437b53748
Brian Drelling <brian@drelling.com>
2022-09-03 17:10:01 -0500

    Added send_notification, added resource exclude

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "Tonic",
     products: [.library(name: "Tonic", targets: ["Tonic"])],
     targets: [
-        .target(name: "Tonic"),
+        .target(name: "Tonic", exclude: ["Tonic.docc"]),
         .testTarget(name: "TonicTests", dependencies: ["Tonic"]),
     ]
 )
